3c9e83e7bea9f8b59c5d1dfae6ac4cb2c337c6e7
Artem <myartoym@gmail.com>
2023-01-18 00:36:37 +0300

    first commit

---
+++ components/header/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main">
+    <div class="logo" />
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">О нас</a></li>
+        <li><a href="#">Контакты</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped lang="scss">
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 100px;
+  width: 100%;
+  box-shadow: 0 5px 5px #dadada;
+}
+
+.logo {
+  background-image: url(~/assets/ico/logo.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  width: 100px;
+  height: 50px;
+  transition: 0.1s;
+  cursor: pointer;
+  &:hover {
+    filter: drop-shadow(0 4px 2px #9e9e9e);
+  }
+}
+
+.menu {
+  ul {
+    display: flex;
+    gap: 10px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main">
+    <Header />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
