9e24d1a519fb6b232549b9ed4119aaebc913a625
Artem <myartoym@gmail.com>
2023-02-28 22:06:16 +0300

    добавлена яМетрика

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,6 +37,32 @@ onMounted(() => {
     api.isCookie = true;
   } else api.isCookie = false;
 });
+
+const yaMetrica = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(92585151, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`;
+const yaMetricaNoScript = '<div><img src="https://mc.yandex.ru/watch/92585151" style="position:absolute; left:-9999px;" alt="" /></div>';
+
+// eslint-disable-next-line no-undef
+useHead({
+  script: [{
+    children: yaMetrica,
+    body: true,
+    type: 'text/javascript',
+  }],
+  noscript: [{
+    children: yaMetricaNoScript,
+    body: true,
+  }],
+});
 </script>
 
 <style scoped>
