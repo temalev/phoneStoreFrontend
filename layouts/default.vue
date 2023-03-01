d501a88795a19ef09e16b5579d55ccf3d13ebfb5
Artem <myartoym@gmail.com>
2023-03-01 09:55:55 +0300

    поправлена метрика

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,12 +44,13 @@ const yaMetrica = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(92585151, "init", {
+   ym(92637429, "init", {
         clickmap:true,
         trackLinks:true,
-        accurateTrackBounce:true
+        accurateTrackBounce:true,
+        webvisor:true
    });`;
-const yaMetricaNoScript = '<div><img src="https://mc.yandex.ru/watch/92585151" style="position:absolute; left:-9999px;" alt="" /></div>';
+const yaMetricaNoScript = '<div><img src="https://mc.yandex.ru/watch/92637429" style="position:absolute; left:-9999px;" alt="" /></div>';
 
 // eslint-disable-next-line no-undef
 useHead({
