c27fa669c74d735440e9dd5a29ad5360ee72539d
Artem <myartoym@gmail.com>
2023-03-17 01:03:25 +0300

    тест

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,10 +1,13 @@
 <template>
-  <div v-if="api.products" class="mainProducts">
+  <div>
     <Title>RK-Tech - {{ currentCategory }}</Title>
     <Meta name="description" :content="currentDescription"/>
+  <div v-if="api.products" class="mainProducts">
+
     <CardProduct v-for="product in api.products" :key="product.uuid" :product="product" />
   </div>
   <div v-else class="loader">загрузка</div>
+</div>
 </template>
 
 <script setup>
