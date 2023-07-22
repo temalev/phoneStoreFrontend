e7860a77e8e9de9422f053e5339d1776f4facc46
Artem <myartoym@gmail.com>
2023-07-22 13:18:55 +0300

    Добавлен новый информационный блок

--- components/infoContainers.vue
+++ components/infoContainers.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="mainInfoContainers">
+    <div class="container">
+      <div class="text">
+        <h3 class="gradient">Рассрочка</h3>
+      <p class="textInfo">Нажмите, чтобы узнать подробности</p>
+      </div>
+      <div class="img" />
+    </div>
+
     <div class="row">
       <div class="leftContainer">
         <div class="textInfoContainer">
           <h3 class="header">Оригинальность</h3>
           <p class="textInfo">
-            Вся техника, представленная в нашем магазине, только новая и оригинальная.
+            Вся техника, представленная в нашем магазине, только новая и
+            оригинальная.
           </p>
         </div>
         <div class="imgBackground__leftContainer" />
@@ -15,7 +24,9 @@
         <div class="rightContainer">
           <div class="textInfoContainer">
             <h3 class="header">Оплата</h3>
-            <p class="textInfo">Оплачивайте в той валюте, в которой удобно вам.</p>
+            <p class="textInfo">
+              Оплачивайте в той валюте, в которой удобно вам.
+            </p>
           </div>
           <div class="imgBackground__rightContainer" />
         </div>
@@ -24,8 +35,8 @@
           <div class="textInfoContainer">
             <h3 class="header">Доставка</h3>
             <p class="textInfo">
-              Мы осуществляем курьерскую доставку в Москве и Московской области, а также в г. Рязань
-              с помощью собственного штата курьеров.
+              Мы осуществляем курьерскую доставку в Москве и Московской области,
+              а также в г. Рязань с помощью собственного штата курьеров.
             </p>
           </div>
           <div
@@ -35,12 +46,13 @@
         </div>
       </div>
     </div>
+
     <div class="cost">
       <div class="textContainer">
         <h3>Гарантия лучшей цены</h3>
         <p class="info">
-          «Гарантия лучшей цены» дает вам уверенность в том, что делаете покупку по самой выгодной
-          цене.
+          «Гарантия лучшей цены» дает вам уверенность в том, что делаете покупку
+          по самой выгодной цене.
         </p>
       </div>
       <CustomButton class="btn" name="Как это работает?" />
@@ -92,6 +104,53 @@
   @media (max-width: 900px) {
     height: 275px;
     flex: none;
+  }
+}
+
+.container {
+  background-color: #f9f9fb;
+  // box-shadow: 0 0 10px #d4d4d4;
+  border-radius: 22px;
+  box-sizing: border-box;
+  @media (max-width: 900px) {
+    margin: 12px 20px;
+    min-width: 280px;
+    width: fit-content;
+    box-shadow: 0 0 10px rgb(212 212 212);
+    height: 295px;
+  }
+  cursor: pointer;
+}
+
+.text {
+  padding: 34px;
+
+  @media (max-width: 900px) {
+    font-size: 16px;
+    padding: 24px;
+  }
+  .gradient {
+    display: inline;
+    // background: -webkit-linear-gradient(180deg, #ed6247, #ed5ba0, #647fe7);
+    // -webkit-background-clip: text;
+    // -webkit-text-fill-color: transparent;
+  }
+}
+
+.list {
+  display: flex;
+  justify-content: center;
+}
+
+.img {
+  background-image: url("/images/credit.jpg");
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  max-width: 1500px;
+  height: 200px;
+  @media (max-width: 900px) {
+    height: 60px;
   }
 }
 
