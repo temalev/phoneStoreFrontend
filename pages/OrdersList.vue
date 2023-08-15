471c8ea2ff40c2166a099a8adfe01d0ca69d995c
Artem <myartoym@gmail.com>
2023-08-15 21:07:56 +0300

    поправил таблицу

--- pages/OrdersList.vue
+++ pages/OrdersList.vue
@@ -2,14 +2,14 @@
   <div class="orderList">
     <client-only>
       <el-table :data="api.newOrders" style="width: 100%" scrollable>
-        <el-table-column sortable prop="fullName" label="ФИО" />
-        <el-table-column v-slot="{ row }" prop="items" label="Товары">
+        <el-table-column sortable prop="fullName" label="ФИО" width="200px" />
+        <el-table-column v-slot="{ row }" prop="items" label="Товары" width="120px">
           <div v-for="item in row.items" :key="item.uuid">{{ item.name }}</div>
         </el-table-column>
-        <el-table-column sortable v-slot="{ row }" prop="createdAt" label="Дата заказа">
+        <el-table-column sortable v-slot="{ row }" prop="createdAt" label="Дата заказа" width="100px">
           {{ moment(row.createdAt).format("DD.MM.YYYY") }}
         </el-table-column>
-        <el-table-column prop="deliveryMessage" label="Адрес" />
+        <el-table-column prop="deliveryMessage" label="Адрес" width="220px" />
         <el-table-column v-slot="{ row }" prop="items" label="Стоимость товаров">
           {{ price(row.items) }}
         </el-table-column>
